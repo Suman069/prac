73484ff4d0ffa2216ac40d4924a26db11dcb94e5
UEM <sroy77753@gmail.com>
2025-03-20 14:37:15 +0530

    change done

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -8,9 +8,9 @@ public class SwapNumbers {
         System.out.println("num2 = " + num2);
 
         
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
+        num1 =num1+num2;
+	num2 = num1-num2;
+	num1 = num1-num2; 
 
         System.out.println("After swapping:");
         System.out.println("num1 = " + num1);
