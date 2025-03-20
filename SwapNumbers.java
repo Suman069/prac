f1fae0b4d405e762dccbbcce0eae5673ff63e9cf
UEM <sroy77753@gmail.com>
2025-03-20 15:09:06 +0530

    AB

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -6,14 +6,10 @@ public class SwapNumbers {
         System.out.println("Before swapping:");
         System.out.println("num1 = " + num1);
         System.out.println("num2 = " + num2);
-
-	System.out.println("AAAAAAAAA");
-        System.out.println("BBBBBBBBBB");
-        System.out.println("CCCCCCCCCC");
 	
 
-        
-        num1 =num1+num2;
+
+	num1 =num2;
 	num2 = num1-num2;
 	num1 = num1-num2; 
 
