6c577283f4ab52686f10c0095f907c4fb0bdcd80
Suman Roy <sroy77753@gmail.com>
2025-05-15 20:40:46 +0530

    test

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,6 +1,7 @@
 public class SwapNumbers {
     public static void main(String[] args) {
-         for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
+         for (int i = 9; i >= 1; i--) {
+            System.out.println(10);
+            
         }
-}
+}}
